36cc25e4681f4499a22639f952ccd21d51d1f099
jihye.seong <jihye.seong@ipageon.com>
2022-09-21 18:01:08 +0900

    로그 레벨 + 트레이스 아이디 찍기 완료

--- src/main/java/spring/core/advanced/app/v2/OrderRepositoryV2.java
+++ src/main/java/spring/core/advanced/app/v2/OrderRepositoryV2.java
@@ -1,22 +1,23 @@
-package spring.core.advanced.app.v1;
+package spring.core.advanced.app.v2;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import spring.core.advanced.trace.TraceId;
 import spring.core.advanced.trace.TraceStatus;
 import spring.core.advanced.trace.hellotrace.HelloTraceV2;
 
 @Repository
 @RequiredArgsConstructor
-public class OrderRepositoryV1 {
+public class OrderRepositoryV2 {
 
     private final HelloTraceV2 trace;
 
-    public void save(String itemId) {
+    public void save(TraceId traceId, String itemId) {
 
         TraceStatus status = null;
 
         try {
-            status = trace.begin("OrderRepository.save()");
+            status = trace.beginSync(traceId, "OrderRepository.save()");
 
             // 저장 로직
             if (itemId.equals("ex")) { // 아이디가 ex로 오면 예외를 터트림
@@ -31,8 +32,6 @@ public class OrderRepositoryV1 {
             // 요구사항 - 예외를 꼭 다시 던져줘야 한다.
             // -> catch로 잡으면 정상 흐름 처리 되니까, 다시 던져줘야 오류가 발생함
         }
-
-
     }
 
     private void sleep(int millis) {
--- src/main/java/spring/core/advanced/app/v2/OrderServiceV2.java
+++ src/main/java/spring/core/advanced/app/v2/OrderServiceV2.java
@@ -1,24 +1,25 @@
-package spring.core.advanced.app.v1;
+package spring.core.advanced.app.v2;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.core.advanced.trace.TraceId;
 import spring.core.advanced.trace.TraceStatus;
 import spring.core.advanced.trace.hellotrace.HelloTraceV2;
 
 @Service
 @RequiredArgsConstructor
-public class OrderServiceV1 {
+public class OrderServiceV2 {
 
-    private final OrderRepositoryV1 orderRepository;
+    private final OrderRepositoryV2 orderRepository;
     private final HelloTraceV2 trace;
 
-    public void orderItem(String itemId) {
+    public void orderItem(TraceId traceId, String itemId) {
 
         TraceStatus status = null;
 
         try {
-            status = trace.begin("OrderService.orderItem()");
-            orderRepository.save(itemId);
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
             trace.end(status);
         } catch (Exception e) { // 예외가 터지면 예외를 잡기
             trace.exception(status, e);
@@ -26,7 +27,5 @@ public class OrderServiceV1 {
             // 요구사항 - 예외를 꼭 다시 던져줘야 한다.
             // -> catch로 잡으면 정상 흐름 처리 되니까, 다시 던져줘야 오류가 발생함
         }
-
-        orderRepository.save(itemId);
     }
 }
